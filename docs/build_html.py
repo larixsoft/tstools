51987af2c7aaaec009f04cea610407df9d9fa577
gwang <github@larixsoft.com>
2017-08-26 11:38:06 -0700

    add CMakeLists.txt and re-org the source code files

--- docs/build_html.py
+++ docs/build_html.py
@@ -12,26 +12,26 @@ more often than the buildhtml.py script.
 
 # ***** BEGIN LICENSE BLOCK *****
 # Version: MPL 1.1
-# 
+#
 # The contents of this file are subject to the Mozilla Public License Version
 # 1.1 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
 # http://www.mozilla.org/MPL/
-# 
+#
 # Software distributed under the License is distributed on an "AS IS" basis,
 # WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 # for the specific language governing rights and limitations under the
 # License.
-# 
+#
 # The Original Code is the MPEG TS, PS and ES tools.
-# 
+#
 # The Initial Developer of the Original Code is Amino Communications Ltd.
 # Portions created by the Initial Developer are Copyright (C) 2008
 # the Initial Developer. All Rights Reserved.
-# 
+#
 # Contributor(s):
 #   Amino Communications Ltd, Swavesey, Cambridge UK
-# 
+#
 # ***** END LICENSE BLOCK *****
 
 import os
@@ -42,7 +42,7 @@ def main():
         base,ext = os.path.splitext(name)
         if ext == '.txt':
             print 'Processing',name
-            os.system('rst2html --stylesheet-path=default.css'
+            os.system('rst2html.py --stylesheet-path=default.css'
                       ' --embed-stylesheet %s > %s'%(name,base+'.html'))
 
 
